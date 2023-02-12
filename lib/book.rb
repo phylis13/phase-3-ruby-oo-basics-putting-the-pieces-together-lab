4829e793d11566f60703b1ce54f659389ed05e54
phylis13 <phylisnjeri54@gmail.com>
2023-02-12 22:08:57 +0300

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
 
 
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    def initialize (title)
+         @title = title
+    end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+this_book = Book.new("Ruby Basics")
+this_book.author = "Allan mauko"
+this_book.page_count = 365
+this_book.genre = "Software Development"
+
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
-# Make your shoe class here!
+# Make your shoe class here!class Shoe
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+    def initialize (brand)
+        @brand = brand
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        self.condition = "new"
+    end
+end
+
+shoe = Shoe.new("Chuck_70")
+shoe.color = "Black"
+shoe.size = 10
+shoe.material = "Leather"
+shoe.condition = "brand_new"
+
+   
